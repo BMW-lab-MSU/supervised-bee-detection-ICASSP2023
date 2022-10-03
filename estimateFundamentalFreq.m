529f8fbafa8ce0e7baaafae0c1d609080ca5bb5d
Trevor Vannoy <trevorvannoy@montana.edu>
2022-10-03 15:47:40 -0600

    add from previous project

---
+++ estimateFundamentalFreq.m
@@ -0,0 +1,31 @@
+function fundamental = estimateFundamentalFreq(psd, opts)
+% estimateFundamentalFreq estimate the fundamental frequency in a PSD using the
+% harmonic product spectrum
+%
+%   fundamental = estimateFundamentalFreq(psd) estimates the fundamental
+%   frequency in the one-sided power spectral density magnitude, psd. 
+%
+%   See also harmonicProductSpectrum.
+
+% SPDX-License-Identifier: BSD-3-Clause
+arguments
+    psd (:,:) {mustBeNumeric}
+    opts.UseParallel (1,1) logical = false
+end
+
+fundamental = zeros(height(psd), 1);
+
+hps = harmonicProductSpectrum(psd, 3);
+
+if opts.UseParallel
+    nWorkers = gcp('nocreate').NumWorkers;
+else
+    nWorkers = 0;
+end
+
+parfor (i = 1:height(psd), nWorkers)
+    [~, fundamental(i)] = findpeaks(hps(i,:), 'NPeaks', 1, ...
+        'SortStr', 'descend');
+end
+
+end
