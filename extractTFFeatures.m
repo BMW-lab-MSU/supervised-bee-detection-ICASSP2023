cd9c307ba55c010f601b709e283b63696d855286
Trevor Vannoy <trevorvannoy@montana.edu>
2022-10-05 13:21:03 -0600

    optimize wavelet feature extraction
    - reduce memory usage by not keeping all the wavelet transforms in
      memory
    - optimize runtime performance by creating a single filterbank object

--- extractTFFeatures.m
+++ extractTFFeatures.m
@@ -5,7 +5,14 @@ arguments
 end
 
 [rows,columns] = size(X);
-cwavelet = cell(rows,1);
+
+max_mean = zeros(rows,1,'like',X);
+max_std = zeros(rows,1,'like',X);
+avg_mean = zeros(rows,1,'like',X);
+avg_skewness = zeros(rows,1,'like',X);
+max_peak = zeros(rows,1,'like',X);
+max_diff = zeros(rows,1,'like',X);
+brk_up = zeros(rows,1,'like',X);
 
 if opts.UseParallel
     nWorkers = gcp('nocreate').NumWorkers;
@@ -13,12 +20,41 @@ else
     nWorkers = 0;
 end
 
+% get better performance by using a single filterbank for each loop
+% iteration instead of one per iteration; this was suggested by MATLAB's
+% "Tips" seciton of the cwt documentation
+waveletFilterbank = cwtfilterbank;
+
 parfor(i = 1:rows, nWorkers)
     if(sum(X(i,:),2) ~= 0)
-    cwavelet{i} = abs(cwt(X(i,:)).^2);
+        cwavelet = abs(wt(waveletFilterbank,X(i,:)).^2);
     else
-        cwavelet{i} = zeros(1,1024);
+        cwavelet = zeros(1,1024);
     end
+
+    % features = extractTFStats(cwavelet);
+
+    max_mean(i) = max(mean(cwavelet,2));
+    max_std(i) = max(std(cwavelet,0,2));
+    avg_mean(i) = mean(mean(cwavelet,2));
+    avg_skewness(i) = mean(skewness(cwavelet,1,2));
+    max_peak(i) = max(max(cwavelet));
+    max_diff(i) = max(max(diff(cwavelet)));
+
+    rc = 0;
+    for j = 0:15
+        rc = rc + sum(sum(cwavelet(:,(64*j + 1 : 64*(j+1)))))/(71*64);
+    end
+    brk_up(i) = rc/16;
 end
-features = extractTFStats(cwavelet);
+
+features = table;
+features.max_mean = max_mean;
+features.max_std = max_std;
+features.avg_mean = avg_mean;
+features.avg_skewness = avg_skewness;
+features.max_peak = max_peak;
+features.max_diff = max_diff;
+features.brk_up = brk_up;
+
 end
