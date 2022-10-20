d533a2ef1fe0188c44f4625e7d21a69811b288c1
Trevor Vannoy <trevorvannoy@montana.edu>
2022-10-20 11:15:30 -0600

    Create basic 2D CNN cross validation code
    We still need to actually tune some parameters, such as weights.

---
+++ tuneCnn2d.m
@@ -0,0 +1,30 @@
+% SPDX-License-Identifier: BSD-3-Clause
+%% Setup
+rng(0, 'twister');
+
+datadir = '../data';
+
+% if isempty(gcp('nocreate'))
+%     parpool('IdleTimeout', Inf);
+% end
+
+%% Load data
+load([datadir filesep 'training' filesep 'trainingData.mat']);
+
+
+%% "tune" parameters; for now, just checking if the framework works...
+
+% empty struct for now since we aren't tuning anything
+hyperparams = struct();
+
+[objective, ~, userdata] = cvCnn2dObjFun(@fitCnn2d, hyperparams, ...
+    crossvalPartition, trainingData, trainingLabels, ...
+    'Progress', true, 'UseParallel', true);
+
+
+result.objective = objective;
+result.userdata = userdata;
+[~, minIdx] = min(result.objective);
+
+% save([datadir filesep 'training' filesep 'augCostTuningADA.mat'],...
+    % 'result', 'result', '-v7.3');
