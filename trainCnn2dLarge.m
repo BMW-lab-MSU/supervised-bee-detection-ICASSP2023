253c5d51ff09e1a910c6cf7481af3f9e2811b7d4
Trevor Vannoy <trevorvannoy@montana.edu>
2022-10-27 14:04:00 -0600

    add 2d cnn training

---
+++ trainCnn2dLarge.m
@@ -0,0 +1,37 @@
+basedir = '../data';
+
+if isempty(gcp('nocreate'))
+    parpool('IdleTimeout', Inf);
+end
+
+%% Load relevant files
+load([basedir filesep 'training' filesep 'tuningCNN2dLarge']);            %"result"
+load([basedir filesep 'training' filesep 'trainingData']);
+
+%% extract the optimal hyperparameter values
+undersamplingRatio=result.UndersamplingRatio;
+costRatio=result.CostRatio;
+
+%% create hyperparameter structure
+
+hyperparams.UndersamplingRatio = undersamplingRatio;
+hyperparams.CostRatio=costRatio;    %used to calculate weight vector in cvobjfun.m
+hyperparams.Cost=[1,costRatio];
+hyperparams.Cost = hyperparams.Cost / mean(hyperparams.Cost);
+
+%% Undersample the majority class
+idxRemove = randomUndersample(...
+    imageLabels(training(holdoutPartition)), 0, ...
+    'UndersamplingRatio', undersamplingRatio, ...
+    'Reproducible', true);
+
+trainingFeatures(idxRemove) = [];
+trainingData(idxRemove) = [];
+trainingLabels(idxRemove) = [];
+
+
+%% train the model
+model = fitCnn2dLarge(trainingData, trainingLabels, hyperparams);
+
+mkdir([basedir filesep 'training' filesep 'models']);
+save([basedir filesep 'training' filesep 'models' filesep 'Cnn2dLarge.mat'] ,"model")
